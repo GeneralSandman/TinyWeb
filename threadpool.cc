11686968299d87b5c443c55674164738c3432b0b
GeneralSandman <13287069774@163.com>
2017-09-10 21:13:13 +0800

    add pool

--- threadpool.cc
+++ threadpool.cc
@@ -1,17 +1,52 @@
 #include "thread.h"
 #include "threadpool.h"
 #include "currentthread.h"
+#include "mutex.h"
 #include "condition.h"
 
 #include <boost/bind.hpp>
 
-void ThreadPool::m_fRunInThread() {}
-Task ThreadPool::m_fTake() {}
+void ThreadPool::m_fRunInThread()
+{
+    //need handle exception
+    while (m_nRunning)
+    {
+        Task t = m_fTake();
+        if (t)
+            t();
+    }
+}
 
-ThreadPool::ThreadPool(const std::string &n)
+Task ThreadPool::m_fTake()
+{
+    MutexLockGuard lock(m_nMutexLock);
+    while (m_nQueue.empty() && m_nRunning)
+    {
+        m_nCondition_notEmpty.wait();
+    }
+    Task t;
+    // always use a while-loop, due to spurious wakeup
+    if (!m_nQueue.empty())
+    {
+        t = m_nQueue.front();
+        m_nQueue.pop_front();
+        m_nCondition_notFull.notify();
+    }
+    return t;
+    // t may be nullptr
+}
+
+bool ThreadPool::m_fIsFull()
+{
+    return m_nQueue.size() == m_nMaxQueueSize;
+}
+
+ThreadPool::ThreadPool(const std::string &n,
+                       int maxqueuesize)
     : m_nName(n),
       m_nCondition_notEmpty(m_nMutexLock),
       m_nCondition_notFull(m_nMutexLock),
+      m_nMaxQueueSize(maxqueuesize),
       m_nRunning(false)
 {
     //m_nQueue init
@@ -38,6 +73,56 @@ void ThreadPool::start(int n)
     }
     m_nRunning = true;
 }
-void ThreadPool::stop() {}
-void ThreadPool::run(const Task &) {}
-ThreadPool::~ThreadPool() {}
+void ThreadPool::stop()
+{
+    //
+    {
+        MutexLockGuard lock(m_nMutexLock);
+        m_nRunning = false;
+        //what should we do?
+        //don't finished
+        //not like muduo ,think why
+        m_nCondition_notEmpty.notifyAll();
+        m_nCondition_notFull.notifyAll();
+        //weakup all threads which is wait()
+    }
+    for_each(m_nThreads.begin(),
+             m_nThreads.end(),
+             boost::bind(&Thread::join, _1));
+}
+bool ThreadPool::run(const Task &t)
+{
+    //if task not been pushed or not been invoked
+    //return false
+
+    //if no child thread
+    //main thread will do task
+    //else:push
+    if (m_nThreads.empty())
+    {
+        t();
+        return true;
+    }
+    else
+    {
+        MutexLockGuard lock(m_nMutexLock);
+        while (m_fIsFull() && m_nRunning)
+        {
+            m_nCondition_notFull.wait();
+        }
+
+        if (!m_fIsFull())
+        {
+            m_nQueue.push_back(t);
+            m_nCondition_notEmpty.notify();
+            return true;
+        }
+    }
+
+    return false;
+}
+ThreadPool::~ThreadPool()
+{
+    if (m_nRunning)
+        stop();
+}
--- threadpool.h
+++ threadpool.h
@@ -16,26 +16,29 @@ typedef boost::function<void()> Task;
 
 class ThreadPool : boost::noncopyable
 {
-  private:
-    std::string m_nName;
-    MutexLock m_nMutexLock;
-    Condition m_nCondition_notEmpty;
-    Condition m_nCondition_notFull;
-    std::deque<Task> m_nQueue;
-    boost::ptr_vector<Thread> m_nThreads;
-    bool m_nRunning;
+private:
+  std::string m_nName;
+  MutexLock m_nMutexLock;
+  Condition m_nCondition_notEmpty;
+  Condition m_nCondition_notFull;
+  int m_nMaxQueueSize;
+  std::deque<Task> m_nQueue;
+  boost::ptr_vector<Thread> m_nThreads;
+  bool m_nRunning;
 
-    void m_fRunInThread();
-    Task m_fTake();
+  void m_fRunInThread();
+  Task m_fTake();
+  bool m_fIsFull();
 
-  public:
-    //constructer don't create thread
-    //start(n) create n theads
-    explicit ThreadPool(const std::string &a = "");
-    void start(int);
-    void stop();
-    void run(const Task &);
-    ~ThreadPool();
+public:
+  //constructer don't create thread
+  //start(n) create n theads
+  explicit ThreadPool(const std::string &a = "",
+                      int maxqueuesize = 100);
+  void start(int);
+  void stop();
+  bool run(const Task &);
+  ~ThreadPool();
 };
 
 #endif // !THREAD_POOL_H
