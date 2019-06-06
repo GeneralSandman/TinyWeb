f4acd825a6dff3e60c48181cfd15e216401077fa
GeneralSandman <13287069774@163.com>
2019-06-06 16:57:02 +0800

    updata (cache)

--- src/tiny_http/http_model_cache.cc
+++ src/tiny_http/http_model_cache.cc
@@ -14,6 +14,125 @@
 #include <tiny_base/log.h>
 #include <tiny_http/http_model_cache.h>
 
+int writeHeaders(disk_cache_t* cache, unsigned int code, const std::string& message)
+{
+    if (nullptr == cache)
+        return -1;
+
+    int return_val;
+
+    sdstr res;
+    sdsnewempty(&res, 1024);
+
+    sdscatsprintf(&res, "HTTP/1.1 %3d %s", code, message);
+
+    char* location = "http://dissigil.cn/ss.html";
+    sdscatsprintf(&res, "\r\nTinyWeb-Cache-Location: %s", location);
+
+    time_t access = 0;
+    sdscatsprintf(&res, "\r\nTinyWeb-Cache-Access: ");
+    formatHttpTime(access, &res);
+
+    time_t data = 0;
+    sdscatsprintf(&res, "\r\nTinyWeb-Cache-Date: ");
+    formatHttpTime(data, &res);
+
+    unsigned int body_offset = res.len + 2 + 27 + 7 + 2 + 2;
+    sdscatsprintf(&res, "\r\nTinyWeb-Cache-Body-Offset: %7d", body_offset);
+
+    sdscat(&res, "\r\n\r\n");
+
+    std::cout << "body offset:" << body_offset << "-" << res.len << std::endl;
+
+    int fd = 1;
+again:
+    return_val = write(fd, res.data, res.len);
+    if (return_val < 0 && errno == EINTR)
+        goto again;
+    if (return_val < res.len)
+        goto fail;
+
+    // TODO:copy to chain.
+    // invoke after-callback write to file.
+    sdsdestory(&res);
+    return 0;
+fail:
+    sdsdestory(&res);
+    return -1;
+}
+
+#if (SYSTEM_WRITEV)
+
+int writeBody(disk_cache_t* cache, chain_t* body)
+{
+    if (nullptr == body)
+        return 0;
+
+    unsigned int chain_len;
+    chain_t* tmp = body;
+    buffer_t* buffer = nullptr;
+    int return_val;
+
+    chain_len = countChain(body);
+    struct iovec* iovs = new struct iovec[chain_len];
+    int index = 0;
+    while (nullptr != tmp) {
+        buffer = tmp->buffer;
+        if (nullptr != buffer) {
+            iovs[index].iov_base = buffer->begin;
+            iovs[index].iov_len = buffer->used - buffer->begin;
+        } else {
+            iovs[index].iov_base = 0;
+            iovs[index].iov_len = 0;
+        }
+        index++;
+        tmp = tmp->next;
+    }
+
+    int fd = 1;
+    return_val = writev(fd, iovs, chain_len);
+
+    if (return_val < 0 && errno == EINTR) {
+        go again;
+        continue;
+    }
+    if (return_val < buffer->used - buffer->begin) {
+        return -1;
+    }
+}
+
+#else // using write() store chain.
+
+int writeBody(disk_cache_t* cache, chain_t* body)
+{
+    if (nullptr == body)
+        return 0;
+
+    chain_t* tmp = body;
+    buffer_t* buffer = nullptr;
+    int return_val;
+    int fd = 1;
+
+    while (nullptr != tmp) {
+        buffer = tmp->buffer;
+        if (nullptr != buffer) {
+            return_val = write(fd, buffer->begin, buffer->used - buffer->begin);
+            if (return_val < 0 && errno == EINTR) {
+                continue;
+            }
+            if (return_val < buffer->used - buffer->begin) {
+                return -1;
+            } else if (return_val == buffer->used - buffer->begin) {
+                tmp = tmp->next;
+            }
+        } else {
+            tmp = tmp->next;
+        }
+    }
+}
+
+#endif
+
 void CacheHeap::m_fFloat(int i)
 {
     int index = i;
@@ -47,7 +166,7 @@ void CacheHeap::m_fSink(int i)
 
 void CacheHeap::m_fSwap(int parent, int child)
 {
-    cache_t* tmp = m_nVector[parent];
+    disk_cache_t* tmp = m_nVector[parent];
     m_nVector[parent] = m_nVector[child];
     m_nVector[child] = tmp;
 
@@ -58,22 +177,22 @@ void CacheHeap::m_fSwap(int parent, int child)
 CacheHeap::CacheHeap()
 {
     LOG(Debug) << "class CacheHeap constructor\n";
-    cache_t* min = new cache_t;
+    disk_cache_t* min = new disk_cache_t;
     min->invalid_time = INT_MAX;
     min->used_times = INT_MAX;
     min->index = 0;
     m_nVector.push_back(min);
 }
 
-void CacheHeap::push(cache_t* file)
+void CacheHeap::push(disk_cache_t* cache)
 {
-    file->index = m_nVector.size() - 1;
-    m_nVector.push_back(file);
-    maps[file->file_name] = file;
-    m_fFloat(m_nVector.size() - 1);
+    cache->index = m_nVector.size() - 1;
+    m_nVector.push_back(cache);
+    maps[cache->location] = cache;
+    m_fFloat(cache->index);
 }
 
-cache_t* CacheHeap::top()
+disk_cache_t* CacheHeap::top()
 {
     if (m_nVector.size() == 1) {
         return nullptr;
@@ -98,7 +217,7 @@ int CacheHeap::changeNode(const std::string& file, unsigned int add_used)
         return -1;
     }
 
-    cache_t* cache = maps[file];
+    disk_cache_t* cache = maps[file];
     cache->used_times += add_used;
     unsigned int index = cache->index;
     m_fSink(index);
@@ -117,8 +236,31 @@ HttpModelCache::HttpModelCache()
     LOG(Debug) << "class HttpModelCache constructor\n";
 }
 
+void HttpModelCache::push(disk_cache_t* cache)
+{
+    if (nullptr == cache) {
+        return;
+    }
+    heap.push(cache);
+    all_cache_size += cache->size;
+}
+
+disk_cache_t* HttpModelCache::remove()
+{
+    disk_cache_t* result = nullptr;
+    result = heap.top();
+    heap.pop();
+    if (nullptr != result)
+        all_cache_size -= result->size;
+    return result;
+}
+
+void HttpModelCache::addUsedTime(const std::string& file)
+{
+    heap.changeNode(file, 1);
+}
+
 HttpModelCache::~HttpModelCache()
 {
     LOG(Debug) << "class HttpModelCache destructor\n";
 }
-
--- src/tiny_http/http_model_cache.h
+++ src/tiny_http/http_model_cache.h
@@ -14,6 +14,8 @@
 #ifndef HTTP_MODEL_CACHE_H
 #define HTTP_MODEL_CACHE_H
 
+#include <tiny_base/memorypool.h>
+#include <tiny_http/http_time.h>
 #include <tiny_struct/sdstr_t.h>
 
 #include <climits>
@@ -23,16 +25,30 @@
 
 #define MAX_DISK_CACHE_SIZE (50 * 1024 * 1024 * 1024)
 
-typedef struct cache_t {
-    std::string file_name;
+typedef struct disk_cache_t {
+    char* location; // url location.
+    char* filename; // filename for disk cache.
+
+    chain_t* source;
+
+    unsigned int body_offset; // body offset in cache file.
+    unsigned int body_length; // body length in cache file.
+
+    unsigned int size; // header_length + body_length;
+
+    time_t age;
+    time_t access;
+    time_t data;
+    time_t last_modified;
+    time_t expires;
+
     time_t invalid_time;
-    unsigned int used_times;
-    struct stat file_info;
+    unsigned int used_times; // for client.
 
-    unsigned int index;
-} cache_t;
+    unsigned int index; // index in cache heap.
+} disk_cache_t;
 
-inline bool cache_comp(const cache_t* a, const cache_t* b)
+inline bool cache_comp(const disk_cache_t* a, const disk_cache_t* b)
 {
     if (a->invalid_time != b->invalid_time) {
         return a->invalid_time < b->invalid_time;
@@ -41,11 +57,14 @@ inline bool cache_comp(const cache_t* a, const cache_t* b)
     }
 }
 
+int writeHeaders(disk_cache_t* cache, unsigned int code, const std::string& message);
+int writeBody(disk_cache_t* cache, chain_t* body);
+
 class CacheHeap {
 
 private:
-    std::vector<cache_t*> m_nVector;
-    std::unordered_map<std::string, cache_t*> maps;
+    std::vector<disk_cache_t*> m_nVector;
+    std::unordered_map<std::string, disk_cache_t*> maps;
 
     void m_fFloat(int i);
     void m_fSink(int i);
@@ -53,122 +72,27 @@ private:
 
 public:
     CacheHeap();
-    void push(cache_t* file);
-    cache_t* top();
+    void push(disk_cache_t* file);
+    disk_cache_t* top();
     void pop();
     int changeNode(const std::string& file, unsigned int add_used);
     ~CacheHeap();
 };
 
 class HttpModelCache {
+
 private:
     CacheHeap heap;
     unsigned long long all_cache_size;
 
 public:
     HttpModelCache();
-    void push(cache_t* cache)
-    {
-        if (nullptr == cache) {
-            return;
-        }
-        heap.push(cache);
-        all_cache_size += cache->file_info.st_size;
-    }
 
-    cache_t* remove()
-    {
-        cache_t* result = nullptr;
-        result = heap.top();
-        heap.pop();
-        if (nullptr != result)
-            all_cache_size -= result->file_info.st_size;
+    void push(disk_cache_t* cache);
+    disk_cache_t* remove();
+    void addUsedTime(const std::string& file);
 
-        return result;
-    }
-
-    void addUsedTime(const std::string& file)
-    {
-        heap.changeNode(file, 1);
-    }
-
-    void test()
-    {
-        cache_t* a1 = new cache_t;
-        cache_t* a2 = new cache_t;
-        cache_t* a3 = new cache_t;
-        cache_t* a4 = new cache_t;
-        cache_t* a5 = new cache_t;
-        cache_t* a6 = new cache_t;
-        cache_t* a7 = new cache_t;
-        cache_t* a8 = new cache_t;
-        cache_t* a9 = new cache_t;
-        cache_t* a10 = new cache_t;
-
-        a1->file_name = "a1";
-        a1->invalid_time = 1;
-        a1->used_times = 1;
-        push(a1);
-
-        a2->file_name = "a2";
-        a2->invalid_time = 2;
-        a2->used_times = 1;
-        push(a2);
-
-        a3->file_name = "a3";
-        a3->invalid_time = 3;
-        a3->used_times = 1;
-        push(a3);
-
-        a4->file_name = "a4";
-        a4->invalid_time = 5;
-        a4->used_times = 4;
-        push(a4);
-
-        a5->file_name = "a5";
-        a5->invalid_time = 5;
-        a5->used_times = 5;
-        push(a5);
-
-        a6->file_name = "a6";
-        a6->invalid_time = 5;
-        a6->used_times = 3;
-        push(a6);
-
-        a7->file_name = "a7";
-        a7->invalid_time = 6;
-        a7->used_times = 10;
-        push(a7);
-
-        a8->file_name = "a8";
-        a8->invalid_time = 5;
-        a8->used_times = 10;
-        push(a8);
-
-        a9->file_name = "a9";
-        a9->invalid_time = 6;
-        a9->used_times = 100;
-        push(a9);
-
-        a10->file_name = "a10";
-        a10->invalid_time = 7;
-        a10->used_times = 99;
-        push(a10);
-
-        addUsedTime("a6");
-        addUsedTime("a6");
-        addUsedTime("a6");
-
-        for (auto i = 0; i < 10; i++) {
-            cache_t* file = remove();
-            if (nullptr == file) {
-                break;
-            }
-            std::cout << file->file_name << ":"
-                      << file->invalid_time << "-" << file->used_times << std::endl;
-        }
-    }
     ~HttpModelCache();
 };
 
-#endif // !MEMORY_POOL_H
+#endif // !HTTP_MODEL_CACHE_H
